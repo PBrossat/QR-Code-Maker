bf920ab1fc0fa6721061b52a47116028cfd57689
Pierrick Brossat <pierrick.brossat@etu.univ-lyon1.fr>
2023-12-01 09:20:30 +0100

    Map gestion d'erreur
    Plus de restrictions sur les parametres d'entree.
    Ajout de la libriarie colorama pour colorer les messages d'erreur (red).
    Maj instruction dans le main.

--- qr.py
+++ qr.py
@@ -1,51 +1,76 @@
 import qrcode
 import os
 from PIL import Image
+from colorama import init, Fore
+
 
 # colors
 white = (255, 255, 255) 
 black = (0, 0, 0) 
 
+init(autoreset=True) # colorama init
 
 # function to verify the parameters
 def verif_params(qr_link,box_size, border,qr_name, box_color, background_color, version,image, image_path):
     if not isinstance(qr_link, str):
-        raise TypeError("qr_link must be a string")
+        raise TypeError(f"{Fore.RED}qr_link must be a string")
 
     if not isinstance(box_size, int):
-        raise TypeError("box_size must be an integer")
+        raise TypeError(f"{Fore.RED}box_size must be an integer")
+    else :
+        if box_size < 1:
+            raise ValueError(f"{Fore.RED}box_size must be greater than 0")
     
     if not isinstance(border, int):
-        raise TypeError("border must be an integer")
+        raise TypeError(f"{Fore.RED}border must be an integer")
+    else :
+        if border < 0:
+            raise ValueError(f"{Fore.RED}border must be greater or equal to 0")
     
     if not isinstance(qr_name, str):
-        raise TypeError("qr_name must be a string")
+        raise TypeError(f"{Fore.RED}qr_name must be a string")
     
     if not isinstance(box_color, tuple):
-        raise TypeError("box_color must be a tuple")
-    
+        raise TypeError(f"{Fore.RED}box_color must be a tuple")
+    else : 
+        if len(box_color) != 3:
+            raise ValueError(f"{Fore.RED}box_color must be a tuple of 3 elements")
+        else : 
+            # 0-255 for RGB
+            if box_color[0] < 0 or box_color[0] > 255:
+                raise ValueError(f"{Fore.RED}box color red value(R-.-.) must be between 0 and 255")
+            if box_color[1] < 0 or box_color[1] > 255:
+                raise ValueError(f"{Fore.RED}box color green value(.-G-.) must be between 0 and 255")
+            if box_color[2] < 0 or box_color[2] > 255:
+                raise ValueError(f"{Fore.RED}box color blue value(.-.-B) must be between 0 and 255")
+            
     if not isinstance(background_color, tuple):
-        raise TypeError("background_color must be a tuple")
+        raise TypeError(f"{Fore.RED}background_color must be a tuple")
+    else :
+        if len(background_color) != 3:
+            raise ValueError(f"{Fore.RED}background_color must be a tuple of 3 elements")
+        else : 
+            # 0-255 for RGB
+            if background_color[0] < 0 or background_color[0] > 255:
+                raise ValueError(f"{Fore.RED}background color red value(R-.-.) must be between 0 and 255")
+            if background_color[1] < 0 or background_color[1] > 255:
+                raise ValueError(f"{Fore.RED}background color green value(.-G-.) must be between 0 and 255")
+            if background_color[2] < 0 or background_color[2] > 255:
+                raise ValueError(f"{Fore.RED}background color blue value(.-.-B) must be between 0 and 255")
     
     if not isinstance(version, int):
-        raise TypeError("version must be an integer")
-    
-    if(version < 1 or version > 40):
-        raise ValueError("version must be between 1 and 40")
-    
-    if(box_size < 1):
-        raise ValueError("box_size must be greater than 0")
-    
-    if(border < 0):
-        raise ValueError("border must be greater or equal to 0")
+        raise TypeError(f"{Fore.RED}version must be an integer")
+    else : 
+        if version < 1 or version > 40:
+            raise ValueError(f"{Fore.RED}version must be between 1 and 40")
     
     # If image is True we check the image_path parameter
     if image:
         if not isinstance(image_path, str):
-            raise TypeError("image_path must be a string")
+            raise TypeError(f"{Fore.RED}image_path must be a string")
 
         if not os.path.exists(image_path):
-            raise ValueError("image_path must be a valid path")
+            raise ValueError(f"{Fore.RED}image_path must be a valid path")
     
 
 
@@ -96,9 +121,12 @@ def create_qr(qr_link,
     qr_code = qr.make_image(fill_color=box_color, back_color=background_color) # create the QR code image
 
     # If image is True, we add the image in the QR code
-    if(image):
-        img_qr = qr.make_image().convert('RGB')
-        pos = ((img_qr.size[0] - image.size[0]) // 2, (img_qr.size[1] - image.size[1]) // 2)
+    if image :
+        # resize the image to fit in the QR code
+        image_qr = image_qr.resize((qr_code.size[0]//5, qr_code.size[1]//5))
+        # position of the image in the QR code (middle of the QR code)
+        pos = ((qr_code.size[0] - image_qr.size[0]) // 2, (qr_code.size[1] - image_qr.size[1]) // 2)
+        # paste the image in the QR code    
         qr_code.paste(image_qr, pos)
 
     qr_code.save("QR_Code_image/" + qr_name) # save the image
@@ -107,9 +135,9 @@ def create_qr(qr_link,
 
 def main():
     qr_link = input("Entrez le lien du QR code : ")
-    box_size = int(input("Entrez la taille de la boîte (en pixels) : "))
+    box_size = int(input("Entrez la taille d'un pixel du QR code' (en pixels) : "))
 
-    border_choice = input("Voulez-vous entrer une bordure (O/N) ? : ").lower()
+    border_choice = input("Voulez-vous entrer une bordure autour du QR code (O/N) ? : ").lower()
     if border_choice.strip().lower() == 'o':
         border = int(input("Entrez la taille de la bordure autour du QR code : "))
     else:
@@ -117,21 +145,21 @@ def main():
 
     qr_name = input("Entrez le nom du fichier QR code : ")
 
-    box_color_choice = input("Voulez-vous entrer une couleur de boîte (O/N) ? : ").lower()
+    box_color_choice = input("Voulez-vous entrer une couleur de pixel (O/N) ? : ").lower()
     if box_color_choice.strip().lower() == 'o':
-        box_color = tuple(map(int, input("Entrez la couleur de la boîte (R G B séparés par des espaces) : ").split()))
+        box_color = tuple(map(int, input("Entrez la couleur du pixel (R G B séparés par des espaces) : ").split()))
     else:
-        box_color = (0, 0, 0) 
+        box_color = black
 
     background_color_choice = input("Voulez-vous entrer une couleur d'arrière-plan (O/N) ? : ").lower()
     if background_color_choice.strip().lower() == 'o':
         background_color = tuple(map(int, input("Entrez la couleur de l'arrière-plan (R G B séparés par des espaces) : ").split()))
     else:
-        background_color = (255, 255, 255)  
+        background_color = white
 
-    version_choice = input("Voulez-vous entrer une version (plus la version est grande plus le qr code sera grand) (O/N) ? : ").lower()
+    version_choice = input("Voulez-vous entrer une précision (plus la précision est grande plus le qr code sera grand) (O/N) ? : ").lower()
     if version_choice.strip().lower() == 'o':
-        version = int(input("Entrez la version du QR code : "))
+        version = int(input("Entrez la taille du QR code : "))
     else:
         version = 3  
 
